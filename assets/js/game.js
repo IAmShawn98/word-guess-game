ca518590ffbeb678cacd84d33e54880b16e9b3f5
icepants <shawnluther99@gmail.com>
2019-05-11 23:43:30 -0400

    Added instruction text to the modal, adjusted the letters used counter and added randomized words

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,20 @@ var wins = 0; // Number of wins.
 var losses = 0; // Number of losses.
 var Guesses = 13; // Number of guesses.
 var guessesMade = 0; //  Number of guesses made.
+var isPlayingGame = false;
+var selctedWord = "";
+var mysteryWord = [];
+// Word list for users to guess from.
+var wordList = ["rascal", "lapdog", "bootcamp", "tobyisdenzel"];
 
 // DOM storage variables.
 var userGuesses = document.getElementById("userGuesses"); // Store how many words are left to guess.
+//var userGuesses = [];
+
 var lettersPicked = document.getElementById("lettersPicked"); // Store how many letters the user guessed.
 var totalGuessCounter = document.getElementById("totalGuessCounter"); // Store the number of total letters the user guessed.
+var generateRandomWord = document.getElementById("generateRandomWord"); // Store random words.
+
 
 // This builds the game when the user clicks play.
 function startGame() {
@@ -25,52 +34,66 @@ document.getElementById("startGame").addEventListener("click", startGame);
 
 // Initializes core game functionality when called for.
 function Gameinit() {
-    document.onkeyup = function (event) {
-        // Store the keys.
-        var userKeyPressEvent = event.key;
-        // Only listen for letters 'A' through 'Z'.
-        if (userKeyPressEvent === 'a' | userKeyPressEvent === 'b' | userKeyPressEvent === 'c' | userKeyPressEvent === 'd'
-            | userKeyPressEvent === 'e' | userKeyPressEvent === 'f' | userKeyPressEvent === 'g' | userKeyPressEvent === 'h'
-            | userKeyPressEvent === 'i' | userKeyPressEvent === 'j' | userKeyPressEvent === 'k' | userKeyPressEvent === 'l'
-            | userKeyPressEvent === 'm' | userKeyPressEvent === 'n' | userKeyPressEvent === 'o' | userKeyPressEvent === 'p'
-            | userKeyPressEvent === 'q' | userKeyPressEvent === 'r' | userKeyPressEvent === 's' | userKeyPressEvent === 't'
-            | userKeyPressEvent === 'u' | userKeyPressEvent === 'v' | userKeyPressEvent === 'w' | userKeyPressEvent === 'x'
-            | userKeyPressEvent === 'y' | userKeyPressEvent === 'z') {
+    isPlayingGame = true;
+    // Reset all game variavles.
+    wins = 0;
+    losses = 0;
+    Guesses = 13;
+    guessesMade = 0;
+    selectedWord = wordList[Math.floor(wordList.length * Math.random())];
+    var hiddenWord = [];
 
-            // Show how many guesses the user has left.
-            Guesses--;
-            // Show how many guesses the user made.
-            guessesMade++;
-            // Populate remaining user guesses.
-            userGuesses.textContent = Guesses;
-            // Populate letters the user guessed.
-            totalGuessCounter.textContent = guessesMade;
-            // Populate all letters typed.
-            lettersPicked.textContent += userKeyPressEvent + " ";
+    // Generate the intial display of the random word.
+    for (var i = 0; i < selectedWord.length; i++) {
+        hiddenWord.push("-");
+    }
+    // Get a reference to the word element.
+    var generateRandomWord = document.getElementById("generateRandomWord");
+    
+    // Populate the hidden word.
+    generateRandomWord.textContent = hiddenWord;
 
-            // If the user runs out of guesses, end the game. (For Now)
-            if (Guesses < 1) {
-                window.location.reload();
-            }
-        }
+    // Replace the "-" with the guessed letters.
+    document.getElementById("generateRandomWord").textContent = hiddenWord.join(" ");
 
-        // Array containing all of our hang states.
-        var hang_state = new Array();
+    
+}
 
-        // First state.
-        hang_state[0] = new Image();
-        hang_state[0].src = './assets/images/hang-states/try-2.jpg';
+document.onkeyup = function (event) {
+    if (!isPlayingGame) {
+        return false;
+    }
 
-        // Second state.
-        hang_state[1] = new Image();
-        hang_state[1].src = './assets/images/hang-states/try-3.jpg';
+    // Store the keys.
+    var userKeyPressEvent = event.key;
+    // Only listen for letters 'A' through 'Z'.
+    if (userKeyPressEvent === 'a' | userKeyPressEvent === 'b' | userKeyPressEvent === 'c' | userKeyPressEvent === 'd'
+        | userKeyPressEvent === 'e' | userKeyPressEvent === 'f' | userKeyPressEvent === 'g' | userKeyPressEvent === 'h'
+        | userKeyPressEvent === 'i' | userKeyPressEvent === 'j' | userKeyPressEvent === 'k' | userKeyPressEvent === 'l'
+        | userKeyPressEvent === 'm' | userKeyPressEvent === 'n' | userKeyPressEvent === 'o' | userKeyPressEvent === 'p'
+        | userKeyPressEvent === 'q' | userKeyPressEvent === 'r' | userKeyPressEvent === 's' | userKeyPressEvent === 't'
+        | userKeyPressEvent === 'u' | userKeyPressEvent === 'v' | userKeyPressEvent === 'w' | userKeyPressEvent === 'x'
+        | userKeyPressEvent === 'y' | userKeyPressEvent === 'z') {
 
-        // Attach a listener to our hang state.
-        var baseImg = document.getElementById("baseImg");
-        // Loop through our hang states each key click.
-        for (var i = 0; i < hang_state.length; i++) {
-            baseImg.appendChild(hang_state[i]);
-            document.getElementById("baseImg").src = (hang_state[i].src);
+        // Show how many guesses the user has left.
+        Guesses--;
+
+        // Show how many guesses the user made.
+        guessesMade++;
+
+        // Populate remaining user guesses.
+        userGuesses.textContent = Guesses;
+
+        // Populate letters the user guessed.
+        totalGuessCounter.textContent = guessesMade;
+
+
+        // Populate letters to the letters guessed box.
+        lettersPicked.textContent += userKeyPressEvent + " ";
+
+        // If the user runs out of guesses, end the game. (For Now)
+        if (Guesses < 1) {
+            window.location.reload();
         }
     }
 }
